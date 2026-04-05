ff14cdc6fe4f5f21fad13b4e7f0f04fc88ccd3fa
nulluna <nulluna@hotmail.com>
2026-04-05 20:47:57 +0800

    clean

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { handleDispatchRequest } from './dispatch'
-import type { DispatchEnv } from './config'
-
-export default {
-  async fetch(request: Request, env: DispatchEnv): Promise<Response> {
-    return handleDispatchRequest(request, env)
-  },
-}
